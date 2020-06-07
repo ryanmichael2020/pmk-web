02da1f8a825618ab89d8e704ee243740ff3535cc
Rick Santos <rick@code-vertical.com>
2020-06-07 15:24:20 +0800

    added missing update employer file from previous commit

---
+++ app/Http/Requests/Employer/UpdateEmployerRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Employer;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
+
+class UpdateEmployerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'employer_id' => 'required|integer',
+            'email' => 'required|email|max:255',
+            'password' => 'string|max:32',
+            'verify_password' => 'required_with:password|string|max:32',
+            'first_name' => 'required|string|max:64',
+            'last_name' => 'required|string|max:32',
+            'sex' => 'required|string|max:16',
+            'company_id' => 'required|integer',
+            'image' => 'image|mimes:jpeg,png,jpg',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        Log::error($validator->getMessageBag());
+        parent::failedValidation($validator);
+    }
+}
